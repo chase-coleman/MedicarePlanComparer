ac8ec9cfb7db0eda3f2be5be2ce80cf9c17e6499
YOUR_NAME <chasehcoleman1@gmail.com>
2025-08-18 13:15:59 -0700

    Initial commit: Spring Boot scaffolding

---
+++ PlanComparison/src/main/java/com/jmcolegroup/PlanComparison/PlanComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.jmcolegroup.PlanComparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanComparisonApplication.class, args);
+	}
+
+}
