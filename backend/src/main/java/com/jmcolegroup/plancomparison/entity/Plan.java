791854c9922efe6d99d3fa30aebd17fec38cd871
YOUR_NAME <chasehcoleman1@gmail.com>
2025-08-21 13:23:00 -0700

    Fixed constraint issue with hospital_stay_length field in Plan entity. Originally had it as unique=true, when it should be false. Hibernate was still tracking it as unique=true even after I removed that. Drop that index from MySQL.

--- backend/src/main/java/com/jmcolegroup/plancomparison/entity/Plan.java
+++ backend/src/main/java/com/jmcolegroup/plancomparison/entity/Plan.java
@@ -26,55 +26,55 @@ public class Plan {
   private String getPlanName() { return planName; }
   private void setPlanName(String planName) {this.planName = planName; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private BigDecimal monthlyPremium;
 
   // monthly premium getter/setter
   private BigDecimal getMonthlyPremium() { return monthlyPremium; }
   private void setMonthlyPremium(BigDecimal monthlyPremium) { this.monthlyPremium = monthlyPremium; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int moop;
 
   // max out of pocket getter/setter
   private int getMoop() { return moop; }
   private void setMoop(int moop) { this.moop = moop; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private String planType;
 
   // plan type getter/setter
   private String getPlanType() { return planType; }
   private void setPlanType(String planType) { this.planType = planType; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int drVisit;
 
   // dr visit getter/setter
   private int getDrVisit() { return drVisit; }
   private void setDrVisit(int drVisit) { this.drVisit = drVisit; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int erVisit;
 
   // ER visit getter/setter
   private int getErVisit() { return erVisit; }
   private void setErVisit(int erVisit) { this.erVisit = erVisit; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int hospitalStay;
 
   // hospital visit getter/setter
   private int getHospitalStay() { return hospitalStay; }
   private void setHospitalStay(int hospitalStay) { this.hospitalStay = hospitalStay; }
 
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false)
   private int hospitalStayLength;
 
   private int getHospitalStayLength() { return hospitalStayLength; }
   private void setHospitalStayLength(int hospitalStayLength) { this.hospitalStayLength = hospitalStayLength; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int surgery;
 
   // surger getter/setter
@@ -82,7 +82,7 @@ public class Plan {
   private void setSurgery(int surgery) { this.surgery = surgery; }
 
   // radiology copay
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int radiologyCopay;
 
   // radiologyCopay getter/setter
@@ -90,28 +90,28 @@ public class Plan {
   private void setRadiologyCopay(int radiologyCopay) { this.radiologyCopay = radiologyCopay; }
 
   // radiology conisurance
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int radiologyCoinsurance;
 
   private int getRadiologyCoinsurance() { return radiologyCoinsurance; }
   private void setRadiologyCoinsurance(int radiologyCoinsurance) { this.radiologyCoinsurance = radiologyCoinsurance; }
 
   // dental benefits
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int dentalBenefit;
 
   // dental benefit setter
   private int getDentalBenefit() { return dentalBenefit; }
   private void setDentalBenefit(int dentalBenefit) { this.dentalBenefit = dentalBenefit; }
 
-  @Column(nullable = false, unique = false)
+  @Column(nullable = false)
   private int otcCredit;
 
   // OTC Credit getter/setter
   private int getOtcCredit() { return otcCredit; }
   private void setOtcCredit(int otcCredit) { this.otcCredit = otcCredit; }
 
-  @Column(nullable = true, unique = false)
+  @Column(nullable = false)
   private String otcRenewal;
 
   // OTC Renewal period
@@ -119,6 +119,7 @@ public class Plan {
   private void setOtcRenewal(String otcRenewal) { this.otcRenewal = otcRenewal; }
 
   // Giveback option
+  @Column(nullable = false)
   private BigDecimal givebackAmount;
 
   // giveback getter/setters
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,26 +4,51 @@ DELETE FROM plan;
 DELETE FROM county;
 DELETE FROM company;
 
--- companies
+-- #######################
+-- ###### Companies ######
+-- #######################
 INSERT INTO company (id, company_name) VALUES (1, "Devoted");
 INSERT INTO company (id, company_name) VALUES (2, "United Healthcare");
 INSERT INTO company (id, company_name) VALUES (3, "Providence");
 
--- plans
+-- ###################
+-- ###### PLANS ######
+-- ###################
+-- Devoted Plans
 INSERT INTO plan (id, plan_name, monthly_premium, moop, plan_type, 
 dr_visit, er_visit, hospital_stay, hospital_stay_length, surgery, 
 radiology_copay, radiology_coinsurance, dental_benefit, 
 otc_credit, otc_renewal, giveback_amount, company_id
 ) VALUES (
-  1, "Core", 0, 5900, "HMO", 0, 125, 425, 4, 525, 0, 20, 1000, 82, "monthly", 0, 1
+  1, "Core", 0, 5900, "HMO", 0, 125, 425, 4, 525, 400, 0, 1000, 82, "monthly", 0, 1
+);
+INSERT INTO plan (id, plan_name, monthly_premium, moop, plan_type, 
+dr_visit, er_visit, hospital_stay, hospital_stay_length, surgery, 
+radiology_copay, radiology_coinsurance, dental_benefit, 
+otc_credit, otc_renewal, giveback_amount, company_id
+) VALUES (
+  2, "Giveback", 0, 7900, "HMO", 0, 110, 375, 5, 475, 300, 0, 250, 0, "None", 137.60, 1
+);
+INSERT INTO plan (id, plan_name, monthly_premium, moop, plan_type, 
+dr_visit, er_visit, hospital_stay, hospital_stay_length, surgery, 
+radiology_copay, radiology_coinsurance, dental_benefit, 
+otc_credit, otc_renewal, giveback_amount, company_id
+) VALUES (
+  3, "Premium", 15.60, 5900, "HMO", 0, 125, 425, 4, 525, 400, 0, 1500, 75, "monthly", 0, 1
 );
 
-
--- counties
+-- ######################
+-- ###### COUNTIES ######
+-- ######################
 INSERT INTO county (id, county_name) VALUES (1, "Linn");
 INSERT INTO county (id, county_name) VALUES (2, "Tillamook");
 INSERT INTO county (id, county_name) VALUES (3, "Lincoln");
 
 
--- plan-county join table
-INSERT INTO counties_plan (plan_id, county_id) VALUES (1, 1);
+-- ####################################
+-- ###### PLAN-COUNTY JOIN TABLE ######
+-- ####################################
+-- Devoted Plans in Lincoln County
+INSERT INTO counties_plan (plan_id, county_id) VALUES (1, 3);
+INSERT INTO counties_plan (plan_id, county_id) VALUES (2, 3);
+INSERT INTO counties_plan (plan_id, county_id) VALUES (3, 3);
