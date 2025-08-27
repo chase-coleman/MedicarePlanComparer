ff5328e0c1d0dcec90b27e20a73dd45d4ae0a189
YOUR_NAME <chasehcoleman1@gmail.com>
2025-08-27 13:57:25 -0700

    Installed heroUI component library.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,3 +1,4 @@
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@heroui/react";
 import React from "react";
 import "../App.css"
 
@@ -5,7 +6,9 @@ const LandingPage = () => {
 
   return (
     <>
-    <div className="testing bg-red-500"></div>
+    <div className="testing bg-red-500">
+
+    </div>
     </>
   )
 }
